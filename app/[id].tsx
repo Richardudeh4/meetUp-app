47c3927e7d2ae6f33709834ba9a2c251801b34ad
richardudeh4 <richardudeh03@gmail.com>
2024-11-08 07:43:31 +0100

    modification to event details page

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,17 +1,31 @@
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams , Stack} from 'expo-router'
 import events from "../assets/events.json";
+import dayjs from 'dayjs';
 
 export default function EventPage() {
   const {id} = useLocalSearchParams();
   const event = events.find((e) => e.id === id);
+
+  if(!event){ 
+    return <Text>Event not found 🥲</Text>
+  }
   return (
-    <View>
-      <Text>
-        event id : {event?.location}
-        
-    </Text>
+    <View className='p-3 bg-white flex-1 gap-3'>
+        <Stack.Screen options={{title: "Event", headerBackTitleVisible: false, headerTintColor: "black"}}/>
+        <Image
+        source={{uri:event.image}} 
+       className='aspect-video w-full rounded-xl'
+      /> 
+         {/* title */}
+         <Text className='font-bold text-3xl' numberOfLines={2}>{event.title}</Text>
+        <Text className='text-lg font-semibold uppercase text-amber-800'>
+        {dayjs(event.datetime).format('ddd, D MMM')}. {dayjs(event.datetime).format('h:mm A' )}
+        </Text>
+
+        <Text className='text-lg' numberOfLines={2}>{event.description}</Text>
+ 
     </View>
   )
 }
